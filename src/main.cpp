b2322c59da909889e7581517d9377c9d4c514426
Pranjal Srivastava <pranjal11246@gmail.com>
2026-07-09 22:05:28 +0530

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -9,12 +9,33 @@ int main() {
   // TODO: Uncomment the code below to pass the first stage
   while(true){
        std::cout << "$ ";
+      std::string input;
       std::string command;
-      std::getline(std::cin,command);
+      std::string parameter;
+      std::getline(std::cin,input);
+
+      command = input.substr(0,input.find(' '));
+      const int parameterSize = input.find(' ')+1;
+
+      if(parameterSize!=std::string::npos){
+        parameter = input.substr(parameterSize);
+      }
+
       if(command == "exit")break;
-      if(command.substr(0,5)=="echo "){
-        std::cout << command.substr(5) << std::endl;
+      if(command=="echo "){
+        std::cout << input.substr(5) << std::endl;
         continue;  
+      }
+
+      if(command == "type "){
+        if(parameter == "echo" || parameter=="exit" || parameter == "type" ){
+          std::cout << parameter << "is a shell builtin" << std::endl;
+          
+        }else{
+          std::cout << parameter << ": not found" << std::endl;
+        }
+        continue;
+        
       }
       std::cout<< command << ": command not found" << std::endl;
   }
