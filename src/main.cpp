00b1b8cab6860079388e365b7cbb51ecc3856fe1
Pranjal Srivastava <pranjal11246@gmail.com>
2026-07-09 21:17:36 +0530

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -11,5 +11,5 @@ int main() {
 
    std::string command;
    std::getline(std::cin,command);
-   std::cout<< command << "command not found" << std::endl;
+   std::cout<< command << ": command not found" << std::endl;
 }
