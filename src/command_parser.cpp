f9a647e48105bdfa3cbd04f404783f9a55a58b34
Pranjal Srivastava <pranjal11246@gmail.com>
2026-07-29 11:53:13 +0000

    codecrafters submit [skip ci]

--- src/command_parser.cpp
+++ src/command_parser.cpp
@@ -16,13 +16,13 @@ RedirectInfo parseRedirection(std::vector<std::string>& tokens){
             tokens.erase(tokens.begin()+i);
             tokens.erase(tokens.begin()+i); 
             return {true,fd,false,filename};
-        }else if(tokens[i]==">>"){
+        }else if(tokens[i]==">>" || tokens[i]=="1>>"){
             fd = STDOUT_FILENO;
             filename = tokens[i+1];
             tokens.erase(tokens.begin()+i);
             tokens.erase(tokens.begin()+i);
             return {true,fd,true,filename};
-        }else if(tokens[i]=="2>"){
+        }else if(tokens[i]=="2>>"){
             fd = STDERR_FILENO;
             filename = tokens[i+1];
             tokens.erase(tokens.begin()+i);
