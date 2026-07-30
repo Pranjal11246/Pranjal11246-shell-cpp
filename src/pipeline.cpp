f9122bdccb4b86ea5b109c238fafe4a9376b68ef
Pranjal Srivastava <pranjal11246@gmail.com>
2026-07-30 15:54:27 +0000

    codecrafters submit [skip ci]

--- src/pipeline.cpp
+++ src/pipeline.cpp
@@ -1,5 +1,5 @@
 #include "pipeline.hpp"
-
+#include <array>
 #include <algorithm>
 #include <vector>
 #include <string>
@@ -53,111 +53,110 @@ std::string findExecutable(const std::string& command)
 
 void executePipeline(const std::vector<std::string>& tokens)
 {
-    auto pipePos = std::find(tokens.begin(), tokens.end(), "|");
-    if (pipePos == tokens.begin() || pipePos + 1 == tokens.end()){
-        std::cerr << "Invalid pipeline\n";
+    std::vector<std::vector<std::string>> commands;
+    std::vector<std::string> current;
+
+    for(int i=0;i<tokens.size();i++){
+
+        if(tokens[i]=="|"){
+
+            if(current.empty()){
+                std::cerr<<"Invalid pipeline\n";
+                return;
+            }
+
+            commands.push_back(current);
+            current.clear();
+        }
+
+        else{
+            current.push_back(tokens[i]);
+        }
+    }
+
+    if(current.empty()){
+        std::cerr<<"Invalid pipeline\n";
         return;
     }
 
-    std::vector<std::string> left(tokens.begin(), pipePos);
-    std::vector<std::string> right(pipePos + 1, tokens.end());
+    commands.push_back(current);
 
-    int pipefd[2];
+    std::vector<std::array<int,2>> pipes;
+    std::vector<pid_t> pids;
 
-    if (pipe(pipefd) == -1)
-    {
-        perror("pipe");
-        return;
+    for(int i=0;i<commands.size()-1;i++){
+
+        std::array<int,2> pipefd;
+
+        if(pipe(pipefd.data())==-1){
+            perror("pipe");
+            return;
+        }
+
+        pipes.push_back(pipefd);
     }
+        for(int i=0;i<commands.size();i++){
 
-    pid_t leftPid = fork();
+        pid_t pid = fork();
 
-    if (leftPid == -1)
-    {
-        close(pipefd[0]);
-        close(pipefd[1]);
+        if(pid==-1){
+            perror("fork");
 
-        perror("fork");
-        return;
-    }
+            for(int j=0;j<pipes.size();j++){
+                close(pipes[j][0]);
+                close(pipes[j][1]);
+            }
 
-    if (leftPid == 0)
-    {
-        if (dup2(pipefd[1], STDOUT_FILENO) == -1){
-            perror("dup2");
+            return;
+        }
+
+        if(pid==0){
+            if(i!=0){
+                if(dup2(pipes[i-1][0],STDIN_FILENO)==-1){
+                    perror("dup2");
+                    _exit(1);
+                }
+            }
+            if(i!=commands.size()-1){
+                if(dup2(pipes[i][1],STDOUT_FILENO)==-1){
+                    perror("dup2");
+                    _exit(1);
+                }
+            }
+            for(int j=0;j<pipes.size();j++){
+                close(pipes[j][0]);
+                close(pipes[j][1]);
+            }
+            if(isBuiltin(commands[i][0])){
+                bool shouldExit=false;
+                executeBuiltin(commands[i],shouldExit);
+                _exit(0);
+            }
+
+            auto args=makeArgs(commands[i]);
+
+            std::string path=findExecutable(commands[i][0]);
+
+            if(path.empty()){
+                std::cerr<<commands[i][0]<<": command not found\n";
+                _exit(127);
+            }
+
+            execv(path.c_str(),args.data());
+
+            perror("execv");
             _exit(1);
         }
 
-        close(pipefd[0]);
-        close(pipefd[1]);
-
-        if (isBuiltin(left[0])){
-            bool shouldExit = false;
-            executeBuiltin(left, shouldExit);
-            _exit(0);
-        }
-
-        auto args = makeArgs(left);
-
-        std::string path = findExecutable(left[0]);
-
-        if (path.empty())
-        {
-            std::cerr << left[0] << ": command not found\n";
-            _exit(127);
-        }
-
-        execv(path.c_str(), args.data());
-
-        perror("execv");
-        _exit(1);
+        pids.push_back(pid);
     }
 
-    pid_t rightPid = fork();
-
-if (rightPid == -1)
-{
-    close(pipefd[0]);
-    close(pipefd[1]);
-    perror("fork");
-    return;
-}
-
-if (rightPid == 0){
-        if (dup2(pipefd[0], STDIN_FILENO) == -1)
-        {
-            perror("dup2");
-            _exit(1);
+        for(int i=0;i<pipes.size();i++){
+        close(pipes[i][0]);
+        close(pipes[i][1]);
         }
 
-        close(pipefd[0]);
-        close(pipefd[1]);
-
-        if (isBuiltin(right[0])){
-            bool shouldExit = false;
-            executeBuiltin(right, shouldExit);
-            _exit(0);
+        for(int i=0;i<pids.size();i++){
+            waitpid(pids[i],nullptr,0);
         }
-
-        auto args = makeArgs(right);
-
-        std::string path = findExecutable(right[0]);
-
-        if (path.empty())
-        {
-            std::cerr << right[0] << ": command not found\n";
-            _exit(127);
-        }
-
-        execv(path.c_str(), args.data());
-
-        perror("execv");
-        _exit(1);
-    }
-
-    close(pipefd[0]);
-    close(pipefd[1]);
-
-    waitpid(leftPid, nullptr, 0);
-    waitpid(rightPid, nullptr, 0);
 }
